15c706f9f72c9ddc7377e80d0e2ae16cbace582c
andji-code <andji121221@gmail.com>
2019-08-28 18:56:27 +0300

    delete tests

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const[arr,setArr]=useState(null);
-  useEffect(()=>{
-    Axios.get('/api/getArr').then(res=>setArr(res.data));
-  })
   return (
     <div className="App">
-      {arr?arr:"no array"}
+      
     </div>
   );
 }
